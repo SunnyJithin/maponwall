ec6b40731dd3ee95590a7192cb031372b63c180a
Jithin Sunny <jithin.sunny@omron.com>
2019-05-20 17:58:29 +0530

    Adding select location screen

---
+++ app/src/main/java/com/jcapp/mapwallpaper/RouteActivity.java
@@ -0,0 +1,16 @@
+package com.jcapp.mapwallpaper;
+
+import android.os.Bundle;
+
+/**
+ * @author Jithin
+ */
+public class RouteActivity extends MapsActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
